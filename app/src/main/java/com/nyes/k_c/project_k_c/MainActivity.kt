00493b49f941dcb4b73fba0fe986398dc25484d4
Kyle Claudio <claudikyle@ecs.vuw.ac.nz>
2018-05-05 00:06:15 +1200

    made an app from Reso Coder's tutorial

--- app/src/main/java/com/nyes/k_c/project_k_c/MainActivity.kt
+++ app/src/main/java/com/nyes/k_c/project_k_c/MainActivity.kt
@@ -5,8 +5,10 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.SeekBar
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +18,39 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Replace with your own action, Please", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        val initialTextViewTranslationY = textViewProgress.translationY
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textViewProgress.text = progress.toString()
+
+                val translationDistance = (initialTextViewTranslationY + progress * resources.getDimension(R.dimen.text_animation_step) * -1)
+                textViewProgress.animate().translationY(translationDistance)
+
+                // if reset is made by button, animate spin
+                if (!fromUser)
+                    textViewProgress.animate().setDuration(500).rotationBy(360f).translationY(initialTextViewTranslationY)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+        // pressing reset buttons sets progress to 0
+        button_reset.setOnClickListener{ view ->
+            seekBar.progress = 0
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
